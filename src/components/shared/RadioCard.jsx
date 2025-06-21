f6035460c7617367803f57a973f90529ff4fe611
Kevin Yoon <brood915@gmail.com>
2025-06-21 19:12:12 -0400

    refactored to remove unnecessary components to use reusable components

---
+++ src/components/shared/RadioCard.jsx
@@ -0,0 +1,23 @@
+const RadioCard = ({ option, isSelected, onSelect }) => {
+  const Icon = isSelected ? option.iconActive : option.icon;
+
+  return (
+    <div
+      className={`border border-solid flex items-center border rounded-lg cursor-pointer transition-all
+        ${isSelected
+          ? "ring-2 ring-[#1849A9]"
+          : "border-[#D1D1D1] bg-white hover:bg-gray-50"}`}
+      onClick={onSelect}
+    >
+      <div
+        className="p-2 rounded-md mr-1"
+      >
+        <Icon size={16}/>
+      </div>
+
+      <span className="flex-1 text-[#242424] text-[14px] font-medium">{option.name}</span>
+    </div>
+  );
+};
+
+export default RadioCard;
